9c80af40eb57e44aa74e35ac6bd86f5f851af783
silentsonata <joon.mink@yahoo.com>
2021-08-05 17:43:15 -0700

    git pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import Particles from "./Particles";
+
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
-      <Particles />
+
     </React.Fragment>
   );
 };
